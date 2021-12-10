74517a6f4a5d7438ded886749740a25d2c64642b
blentell <78812236+blentell@users.noreply.github.com>
2021-12-10 13:30:48 -0500

    Add files via upload

--- meal-planner-app/src/App.js
+++ meal-planner-app/src/App.js
@@ -37,14 +37,58 @@ function App() {
 
 	async function handleMealSelected(mealSelected) {
 		setMealSelected(mealSelected);
+		//console.log("mealSelected: ", mealSelected);
 		setSearching(false);
 		try {
 			let payload = await axios.post(
 				"http://localhost:3001/api/meals/add-meal/",
 				{
 					mealTitle: mealSelected.strMeal,
-					mealPicture: mealSelected.strMealThumb,
-					mealDate: document.querySelector("input .dateInput"),
+					mealPicture: mealSelected.strMealThumb,					
+					mealArea: mealSelected.strArea,
+					mealCategory: mealSelected.strCategory,
+					mealIngredient1: mealSelected.strIngredient1,
+					mealIngredient2: mealSelected.strIngredient2,
+					mealIngredient3: mealSelected.strIngredient3,
+					mealIngredient4: mealSelected.strIngredient4,
+					mealIngredient5: mealSelected.strIngredient5,
+					mealIngredient6: mealSelected.strIngredient6,
+					mealIngredient7: mealSelected.strIngredient7,
+					mealIngredient8: mealSelected.strIngredient8,
+					mealIngredient9: mealSelected.strIngredient9,
+					mealIngredient10: mealSelected.strIngredient10,
+					mealIngredient11: mealSelected.strIngredient11,
+					mealIngredient12: mealSelected.strIngredient12,
+					mealIngredient13: mealSelected.strIngredient13,
+					mealIngredient14: mealSelected.strIngredient14,
+					mealIngredient15: mealSelected.strIngredient15,
+					mealIngredient16: mealSelected.strIngredient16,
+					mealIngredient17: mealSelected.strIngredient17,
+					mealIngredient18: mealSelected.strIngredient18,
+					mealIngredient19: mealSelected.strIngredient19,
+					mealIngredient20: mealSelected.strIngredient20,
+					mealMeasure1: mealSelected.strMeasure1,
+					mealMeasure2: mealSelected.strMeasure2,
+					mealMeasure3: mealSelected.strMeasure3,
+					mealMeasure4: mealSelected.strMeasure4,
+					mealMeasure5: mealSelected.strMeasure5,
+					mealMeasure6: mealSelected.strMeasure6,
+					mealMeasure7: mealSelected.strMeasure7,
+					mealMeasure8: mealSelected.strMeasure8,
+					mealMeasure9: mealSelected.strMeasure9,
+					mealMeasure10: mealSelected.strMeasure10,
+					mealMeasure11: mealSelected.strMeasure11,
+					mealMeasure12: mealSelected.strMeasure12,
+					mealMeasure13: mealSelected.strMeasure13,
+					mealMeasure14: mealSelected.strMeasure14,
+					mealMeasure15: mealSelected.strMeasure15,
+					mealMeasure16: mealSelected.strMeasure16,
+					mealMeasure17: mealSelected.strMeasure17,
+					mealMeasure18: mealSelected.strMeasure18,
+					mealMeasure19: mealSelected.strMeasure19,
+					mealMeasure20: mealSelected.strMeasure20,
+					mealSource: mealSelected.strSource,
+					mealYoutube: mealSelected.strYoutube,
 				},
 				{
 					headers: {
@@ -54,7 +98,7 @@ function App() {
 			);
       // window.location.reload(false);
 			// console.log("AppMealSelected: ", mealSelected);
-			// console.log("payload: ", payload);
+			console.log("Apppayload: ", payload);
 		} catch (e) {
 			console.log(e.response);
 		}
