37ef4c9a0b831e0013d2c23104ee63c0fe477fe8
blentell <78812236+blentell@users.noreply.github.com>
2021-12-15 13:42:48 -0500

    Delete meal-planner-app/context directory
    Wrong location

--- meal-planner-app/context/mealContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const SearchContext = React.createContext({});
-export const RecipeContext = React.createContext({});
-export const NewRecipeContext = React.createContext({});
-
-
